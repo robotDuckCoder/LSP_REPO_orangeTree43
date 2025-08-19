de2b99ceb7579bd61271254088966301bb674687
robotDuckCoder <duckjump3@gmail.com>
2025-08-19 10:52:52 -0400

    First Homework Assignment for Large Scale Programming

---
+++ LSP_REPO_orangeTree43/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Name: Joseph Neisen
+ * 
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
