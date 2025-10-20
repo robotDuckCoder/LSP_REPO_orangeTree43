a1fab78641f108f55470e53a26c654a93254e2a5
robotDuckCoder <duckjump3@gmail.com>
2025-10-20 19:23:54 -0400

    Finished with question 2, now I am starting question 4

---
+++ LSP_REPO_orangeTree43/src/org/howard/edu/lsp/midterm/question2/Main.java
@@ -0,0 +1,26 @@
+package org.howard.edu.lsp.midterm.question2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Circle radius 3.0 → area = " + AreaCalculator.area(3.0));
+		System.out.println("Rectangle 5.0 x 2.0 → area = " + AreaCalculator.area(5.0, 2.0));
+		System.out.println("Triangle base 10, height 6 → area = " + AreaCalculator.area(10, 6));
+		System.out.println("Square side 4 → area = " + AreaCalculator.area(4));
+		
+		try {
+			AreaCalculator.area(-1.0);
+		}
+		catch(IllegalArgumentException e){
+			System.out.println("Error, " + e.toString());
+		}
+	}
+	
+	/*
+	 * Using methods with different names would be the better choice in this circumstance. 
+	 * This is because this way of overloading does not allow the user to calculate the area of a triangle with decimal side lengths. 
+	 * It is also because unless you know the AreaCalculator class, it is not easy to predict what you are finding the area of in this case.
+	 * The code would be much more understandable and usable with methods named areaRectangle(), etc. for all of the different shapes.
+	 */
+
+}
