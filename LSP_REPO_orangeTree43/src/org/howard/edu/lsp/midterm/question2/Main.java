b7ce4a7e92be6da96e3ee1f98241b22ccddd5cbd
robotDuckCoder <duckjump3@gmail.com>
2025-10-20 20:01:07 -0400

    Finished with the coding section of the midterm.

--- LSP_REPO_orangeTree43/src/org/howard/edu/lsp/midterm/question2/Main.java
+++ LSP_REPO_orangeTree43/src/org/howard/edu/lsp/midterm/question2/Main.java
@@ -17,7 +17,8 @@ public class Main {
 	}
 	
 	/*
-	 * Using methods with different names would be the better choice in this circumstance. 
+	 * Using methods with different names would be the better choice in this circumstance, although the current implementation does use overloading.
+	 * You can tell that it uses overloading because there are 4 functions of the same name which are called depending on the parameters that are given to each function.
 	 * This is because this way of overloading does not allow the user to calculate the area of a triangle with decimal side lengths. 
 	 * It is also because unless you know the AreaCalculator class, it is not easy to predict what you are finding the area of in this case.
 	 * The code would be much more understandable and usable with methods named areaRectangle(), etc. for all of the different shapes.
